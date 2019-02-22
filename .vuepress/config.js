7b06918bffe4356767c7eb6a9f0d6943148f1f20
JL Peyret <jpeyret@gmail.com>
2019-02-22 15:55:49 -0800

    "just a backup, use it sparingly on main codelines"

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,32 +1,50 @@
 module.exports = {
-        title: 'Select Objects - thinking about Python, databases and PeopleSoft',
+    title: 'Select Objects - thinking about Python, databases and PeopleSoft',
+    home: true,
+
+
+    // this ends up on the Home page, under the title.
+    description: "SQL beats ORM",
+
+
+    themeConfig: {
+        nav: [
+            { text: 'Services', link: '/services/' },
+            { text: 'About', link: '/about/' }
+        ],
+
         home: true,
 
+        //turns off the search, might as well until configured.
+        search: false,
 
-        themeConfig: {
-            nav: [
-                { text: 'Services', link: '/services/' },
-                { text: 'About', link: '/about/' }
-            ],
+        logo: '/select-objects-logo-small.png',
 
-            home: true,
+        displayAllHeaders: true,
 
-            logo: '/select-objects-logo-small.png',
-
-            displayAllHeaders: true,
-
-            sidebar: [
-                '/',
-                '/application_engine/ae_call_ci_p1/',
-                '/application_engine/ae_call_ci_p2/',
-                '/application_engine/ae_call_ci_p3/'
-            ],
+        sidebar: [
+            '/',
+            '/application_engine/ae_call_ci_p1/',
+            '/application_engine/ae_call_ci_p2/',
+            '/application_engine/ae_call_ci_p3/'
+        ],
 
 
-            features: [{
-                title: "test title1",
-                details: "test details 1"
-            }]
+        features: [{
+            title: "test title1",
+            details: "test details 1"
+        }]
 
-        }
-    }
+    },
+
+    plugins: [
+
+        [
+            '@vuepress/plugin-blog',
+            {
+            }
+        ]
+    ],
+
+
+}
