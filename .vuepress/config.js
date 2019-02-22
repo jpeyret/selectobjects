96aa3496d21567027f98713b4c1b80ad10455455
JL Peyret <jpeyret@gmail.com>
2019-02-22 15:03:38 -0800

    "just a backup, use it sparingly on main codelines"

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,23 +1,32 @@
 module.exports = {
-    title: 'Select Objects - thinking about Python, databases and PeopleSoft',
-    home: true,
+        title: 'Select Objects - thinking about Python, databases and PeopleSoft',
+        home: true,
 
 
-    themeConfig: {
-        nav: [
-            { text: 'Services', link: '/services/' },
-            { text: 'About', link: '/about/' }
-        ],
+        themeConfig: {
+            nav: [
+                { text: 'Services', link: '/services/' },
+                { text: 'About', link: '/about/' }
+            ],
 
-    logo: '/select-objects-logo-small.png',
+            home: true,
 
-        displayAllHeaders: true,
+            logo: '/select-objects-logo-small.png',
 
-        sidebar: [
-            '/',
-            '/application_engine/ae_call_ci_p1/',
-            '/application_engine/ae_call_ci_p2/',
-            '/application_engine/ae_call_ci_p3/'
-        ]
+            displayAllHeaders: true,
+
+            sidebar: [
+                '/',
+                '/application_engine/ae_call_ci_p1/',
+                '/application_engine/ae_call_ci_p2/',
+                '/application_engine/ae_call_ci_p3/'
+            ],
+
+
+            features: [{
+                title: "test title1",
+                details: "test details 1"
+            }]
+
+        }
     }
-}
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ---
-footer: MIT Licensed
+home: true
+heroImage: /select-objects-logo-small.png
+actionText: Get Started →
+<!-- actionLink: /guide/ -->
+features:
+- title: Fast, Free, Secure
+  details: Keeping software lean and mean.
 ---
 
-# Musing about Python, databases and PeopleSoft
